e8082e8a81d7a341899e09db00f20cb4f3393f6d
Mohamed Irfan <i4irfan777@gmail.com>
2023-01-15 17:06:40 +0530

    Implement logging

--- contest2calender.py
+++ contest2calender.py
@@ -2,15 +2,29 @@ import calender
 import kontest
 import todoist
 from time import sleep
+import logging
+import sys
+
+logging.basicConfig(
+        level=logging.DEBUG, 
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
+        datefmt='%d/%m/%Y %H:%M:%S' ,     
+        handlers=[
+        logging.FileHandler("contest2calender.log",'w'),
+        logging.StreamHandler(sys.stdout),
+    ])
+logger = logging.getLogger()
 
 def main():
   sites = ["codeforces","at_coder","code_chef"]
   contests = []
   for site in sites:
+    logger.info(f"Getting available contest for {site}")
     contests.extend(kontest.get_contests(site))
   
   for contest in contests:
     duplicate = False
+    logger.info(f"Checking if {contest.name} already exist")
     urls = calender.list_event_urls(contest.start,contest.end)
     for url in urls:
       if contest.url == url:
@@ -18,11 +32,12 @@ def main():
         break
     if not duplicate:
       print(contest.name)
+      logger.info(f"{contest.name} not found in calender, adding to calender")
       calender.add_event(contest.name,contest.start,contest.end,contest.url)
-  print("Contests added to googel calender\n")
+  logger.info("All contest are added to calender")
   sleep(30)
+  logger.info("Moving todoist inbox tasks to projects/cp")
   todoist.move()
-  print("Contest added to todoist")
 
 if __name__ == '__main__':
     main()
